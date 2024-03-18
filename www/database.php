981c894c575c1ec4ce33c46178025dc050d4ad18
Luca Wijsman <186515@novacollege.nl>
2024-03-18 13:05:32 +0100

    Database connectie

--- www/database.php
+++ www/database.php
@@ -1,3 +1,11 @@
 <?php
-
-//maak een database connectie
+$host = "localhost";
+$user = "root";
+$password = "";
+$databaseName = "";
+$conn = mysqli_connect($host, $user, $password, $databaseName);
+if (mysqli_connect_error()) {
+    echo "Er is een error. Probeer opnieuw.";
+} else {
+    echo "De connectie is gelukt.";
+}
