5cef648ab54309632258ad39113285f0187f0570
Luca Wijsman <186515@novacollege.nl>
2024-03-19 11:16:46 +0100

    Index header aangepast + css erbij

--- www/index.php
+++ www/index.php
@@ -6,12 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Het Receptenboek</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="images/Colombia_land_flag.png">
 </head>
 
 <body>
     <header>
-        <p>Hallo</p>
+        <h1 class="header_title">Colombite</h1>
+        <p class="header_subtext">Het beste eten van Colombia</p>
     </header>
+    <main>
+
+    </main>
 </body>
 
 </html>
