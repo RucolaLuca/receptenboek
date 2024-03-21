309677cf28e1492d013cb38b8ad9b7d0159c352d
Luca Wijsman <186515@novacollege.nl>
2024-03-21 09:28:31 +0100

    bezig met de recepten pagina

---
+++ www/recepten.php
@@ -0,0 +1,45 @@
+<?php
+require "database.php";
+$sql = "SELECT * FROM colombite";
+$result = mysqli_query($conn, $sql);
+$receptenboek = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Receptenlijst</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="https://github.com/RucolaLuca">About Me</a></li>
+            </ul>
+        </nav>
+        <h1 class="recepten_title">Colombite</h1>
+    </header>
+    <main>
+        <div class="div">
+            <div class="recept">
+                <?php foreach ($receptenboek as $recept) : ?>
+                    <tr>
+                        <td><?php echo $recept["id"] ?></td>
+                        <td>
+                            <a href="recept.php?id=<?php echo $recept["id"] ?>">
+                                <img src="<?php echo $recept["afbeelding"] ?>" alt="Recept Afbeelding">
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
