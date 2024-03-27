bfaf7d730804ea68b890e0cf2080191440c5c6e6
Luca Wijsman <186515@novacollege.nl>
2024-03-27 08:43:36 +0100

    footer en main mooier en index text aangepast

--- www/index.php
+++ www/index.php
@@ -14,24 +14,25 @@
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
+                <li><a href="recepten.php">Recepten</a></li>
                 <li><a href="https://github.com/RucolaLuca">About Me</a></li>
                 <li><a href="specials.php">Specials</a></li>
             </ul>
         </nav>
         <h1 class="header_title"><a href="recepten.php" class="header_title">Colombite</a></h1>
-        <p class="header_subtext">Het beste eten van Colombia</p>
+        <p class="header_subtext">Het beste eten uit Colombia.</p>
     </header>
     <main>
         <h2>Wilt u graag eens iets maken uit de Colombiaanse keuken?</h2>
         <h3>Dan bent u hier op de goede plek!</h3>
-        <h3>Hier bij Colombite hebben we de lekkerste recepten voor u!</h3>
+        <h3>Hier bij Colombite hebben we de lekkerste gerechten voor u!</h3>
     </main>
     <footer>
         <div class="person-list">
-            <h3>Maak kennis met het team</h3>
+            <h3>Maak kennis met het team.</h3>
             <ul>
                 <li>Projectleider: Cornelius Arne</li>
-                <li>Begeleider: ICT Academie Nova College Haarlem</li>
+                <li>Begeleiding: ICT Academie Nova College Haarlem</li>
                 <li>Developer: Luca Wijsman</li>
             </ul>
         </div>
